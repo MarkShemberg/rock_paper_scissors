abd900fb5c62d2966a8b0ef47819d759e0f61a1f
MarkShemberg <markshemberg@gmail.com>
2023-10-20 11:25:07 +0800

    Rewrote the js codes to make it work with html.Will do CSS next.

--- scripts.js
+++ scripts.js
@@ -1,78 +1,123 @@
+// GAME
+
+let playerScore = 0;
+let computerScore =0;
+let result = "";
 
 
-let playerScore = 0
-let computerScore = 0
-let gameResult = ""
-let playerSelection = prompt("Rock Paper Scissors","SCisSoRS").toLowerCase();
-let computerSelection = getComputerChoice();
-
-function getPlayerChoice(){
-    
+function getComputerChoice(){
+  let choices = ["✊", "✋", "✌"]
+  return choices[Math.floor(Math.random() * 3)]
 }
 
+function playRound(playerSelection, computerSelection){
 
-function getComputerChoice() {
-    let choices = ["rock", "paper", "scissors"]
-    let result = choices[Math.floor(Math.random() * choices.length)]
-    return result
-}
+  if (playerSelection === computerSelection){
+    result = "Tie"
+  }
 
-function playRound(playerSelection, computerSelection) {
-    let log = "";
-
-    
-    if (
-        (playerSelection === "rock" && computerSelection === "scissors") ||
-        (playerSelection === "paper" && computerSelection === "rock") ||
-        (playerSelection === "scissors" && computerSelection === "paper")
+  else if (
+    (playerSelection === "✊" && computerSelection === "✌") ||
+    (playerSelection === "✋" && computerSelection === "✊") ||
+    (playerSelection === "✌" && computerSelection === "✋")
     ){
-        playerScore++;
-        log = "You Win!";
-        
-    }
+      result = "Win";
+      playerScore++
+  }
 
-    else if (
-        (computerSelection === "rock" && playerSelection === "scissors") ||
-        (computerSelection === "paper" && playerSelection === "rock") ||
-        (computerSelection === "scissors" && playerSelection === "paper")
-    ){
-        computerScore++
-        log = "You Lose!";
-
-       
-    }
-    
-    
-    else {
-        log = "Tie!";
-    }
-    return log
+  else if (
+    (computerSelection === "✊" && playerSelection === "✌") ||
+    (computerSelection === "✋" && playerSelection === "✊") ||
+    (computerSelection === "✌" && playerSelection === "✋")
+   ){
+    result = "Lose";
+    computerScore++
+  }
+  updateMsg(result, playerSelection, computerSelection)
+  console.log(result);
 }
 
 function isGameOver(){
-    let overMsg = "Game is Over!"
-    return overMsg
+  if (playerScore === 5 || computerScore === 5){
+    console.log("Game is Over!");
+    setEndmsg();
+    modal.showModal();
+    
+  }
+}
+
+function updateScore(){
+  if (result === "Tie"){
+    resultText.textContent = "It's a Tie!"
+  } else if (result === "Win"){
+    resultText.textContent = "You Win!"
+  } else if (result === "Lose"){
+    resultText.textContent = "You Lose!"
+  }
+
+  playerScoreP.textContent = `Player: ${playerScore}`
+  computerScoreP.textContent = `Computer: ${computerScore}`
+}
+
+function updateMsg(result, playerSelection, computerSelection){
+  if (result === "Win"){
+    resultMsg.textContent = `${playerSelection} beats ${computerSelection}`
+    return
+  }
+
+  if (result === "Lose"){
+    resultMsg.textContent = `${playerSelection} is beaten by ${computerSelection}`
+    return
+  }
+
+  resultMsg.textContent = `${playerSelection} is tied with ${computerSelection}`
+}
+
+function restartGame(){
+  playerScore = 0
+  computerScore = 0
+  resultText.textContent = "Press a Button to Start"
+  resultMsg.textContent = "First to 5 points Wins!"
+  playerScoreP.textContent = "Player: 0"
+  computerScoreP.textContent = "Computer: 0"
+}
+
+function setEndmsg(){
+  if (playerScore > computerScore){
+    endgameMsg.textContent = "You Won!"
+  }
+  else if (computerScore > playerScore){
+    endgameMsg.textContent = "You Lost!"
+  }
+
 }
 
 
 
-function game(){
-    if (
-        (playerScore != 5) ||
-        (computerScore !=5)
-    ) {
-        playRound(playerSelection, computerSelection);
-    }
 
-    else if (
-        (playerScore === 5) ||
-        (computerScore ===5)
-    ){
-        isGameOver()
-    }
 
-   
+const rockbtn = document.getElementById("rockBtn")
+const paperbtn = document.getElementById("paperBtn")
+const scissorsbtn = document.getElementById("scissorsBtn")
+const playerScoreP = document.getElementById("playerScore")
+const computerScoreP = document.getElementById("computerScore")
+const resultText = document.getElementById("resultText")
+const resultMsg = document.getElementById("resultMsg")
+const restartBtn = document.getElementById("restartBtn")
+const modal = document.getElementById("endgameModal")
+const endgameMsg= document.getElementById("endgameMsg")
+
+rockbtn.addEventListener("click", () => startGame("✊"))
+paperbtn.addEventListener("click", () => startGame("✋"))
+scissorsbtn.addEventListener("click", () => startGame("✌"))
+restartBtn.addEventListener("click", restartGame)
+restartBtn.addEventListener("click",() => {modal.close();})
+
+function startGame(playerSelection){
+  const computerSelection = getComputerChoice();
+  playRound(playerSelection, computerSelection)
+  updateScore()
+  isGameOver()
+  
 }
-   
 
-  console.log(game());
