15eaffc78d06043a7389cda52480bae00cd678c9
MarkShemberg <markshemberg@gmail.com>
2023-10-21 09:57:22 +0800

    This Project is done for now.Will see if i can remake it with animations in the future.

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
-# rock_paper_scissors
+# Rock_Paper_Scissors 
 
-Using only javascript for now
-Will add html elements and css styles in the future
+First Stage | Using Javascript to make the game
+
+• Created functions to randomize computer choice and a prompt to take player input.
+• Made a function to play a round determing who the winner is
+and added scores accordingly.
+• Made a Function to reset scores and text when 5 points is reached by either side.
+
+Second Stage | Adding Html & Css and connecting Js
+
+Html
+• Added a Header, Main, Footer in Html
+• Added elements for displaying result and keeping scores.
+• Added buttons for player choices
+• Added Modal to popup when game is over with restart button.
+
+Javascript
+• Declared variable and connected to Html elements.
+• Added eventListeners to buttons with functions to start and reset game.
+• Made and remade functions to make code less messy.
+
+Css
+
+• Used Flexbox to try and keep things centered.
+• Added Pixel game-like fonts.
+• Change color palette to mostly green shades.
+
+
+
--- scripts.js
+++ scripts.js
@@ -1,15 +1,36 @@
-// GAME
+// Rock Paper Scissors Game
 
+// Declaring Global Variables
 let playerScore = 0;
 let computerScore =0;
 let result = "";
 
+// Declaring variables used to connect to HTML elements 
+const rockbtn = document.getElementById("rockBtn")
+const paperbtn = document.getElementById("paperBtn")
+const scissorsbtn = document.getElementById("scissorsBtn")
+const playerScoreP = document.getElementById("playerScore")
+const computerScoreP = document.getElementById("computerScore")
+const resultText = document.getElementById("resultText")
+const resultMsg = document.getElementById("resultMsg")
+const resetBtn = document.getElementById("resetBtn")
+const modal = document.getElementById("endgameModal")
+const endgameMsg= document.getElementById("endgameMsg")
 
+// Adding Eventlisteners to buttons and connecting functions to clicks
+rockbtn.addEventListener("click", () => startGame("✊"))
+paperbtn.addEventListener("click", () => startGame("✋"))
+scissorsbtn.addEventListener("click", () => startGame("✌"))
+resetBtn.addEventListener("click", resetGame)
+resetBtn.addEventListener("click",() => {modal.close();})
+
+// Function to generate a choice for the computer
 function getComputerChoice(){
   let choices = ["✊", "✋", "✌"]
   return choices[Math.floor(Math.random() * 3)]
 }
 
+// Function to play a round of RPS, used in "click" eventlistener for buttons 
 function playRound(playerSelection, computerSelection){
 
   if (playerSelection === computerSelection){
@@ -37,6 +58,7 @@ function playRound(playerSelection, computerSelection){
   console.log(result);
 }
 
+// Function to end the game after a side reaches 5 points, displays the end message in a modal popup
 function isGameOver(){
   if (playerScore === 5 || computerScore === 5){
     console.log("Game is Over!");
@@ -46,6 +68,7 @@ function isGameOver(){
   }
 }
 
+// Function to update the score on either side and displays whether player wins/lose/ties
 function updateScore(){
   if (result === "Tie"){
     resultText.textContent = "It's a Tie!"
@@ -59,29 +82,32 @@ function updateScore(){
   computerScoreP.textContent = `Computer: ${computerScore}`
 }
 
+// Function to display what both sides chose and who beats who
 function updateMsg(result, playerSelection, computerSelection){
   if (result === "Win"){
-    resultMsg.textContent = `${playerSelection} beats ${computerSelection}`
+    resultMsg.textContent = `${playerSelection} \xa0\xa0\xa0beats\xa0\xa0\xa0 ${computerSelection}`
     return
   }
 
   if (result === "Lose"){
-    resultMsg.textContent = `${playerSelection} is beaten by ${computerSelection}`
+    resultMsg.textContent = `${playerSelection} \xa0\xa0\xa0is beaten by\xa0\xa0\xa0 ${computerSelection}`
     return
   }
 
-  resultMsg.textContent = `${playerSelection} is tied with ${computerSelection}`
+  resultMsg.textContent = `${playerSelection} \xa0\xa0\xa0is tied with\xa0\xa0\xa0 ${computerSelection}`
 }
 
-function restartGame(){
+// Function tied to the resetBtn which resets the value to default. Shows up in the modal popup
+function resetGame(){
   playerScore = 0
   computerScore = 0
   resultText.textContent = "Press a Button to Start"
-  resultMsg.textContent = "First to 5 points Wins!"
+  resultMsg.textContent = "First to Five points Wins!"
   playerScoreP.textContent = "Player: 0"
   computerScoreP.textContent = "Computer: 0"
 }
 
+// Function to change the message which shows up in the modal popup
 function setEndmsg(){
   if (playerScore > computerScore){
     endgameMsg.textContent = "You Won!"
@@ -92,32 +118,11 @@ function setEndmsg(){
 
 }
 
-
-
-
-
-const rockbtn = document.getElementById("rockBtn")
-const paperbtn = document.getElementById("paperBtn")
-const scissorsbtn = document.getElementById("scissorsBtn")
-const playerScoreP = document.getElementById("playerScore")
-const computerScoreP = document.getElementById("computerScore")
-const resultText = document.getElementById("resultText")
-const resultMsg = document.getElementById("resultMsg")
-const restartBtn = document.getElementById("restartBtn")
-const modal = document.getElementById("endgameModal")
-const endgameMsg= document.getElementById("endgameMsg")
-
-rockbtn.addEventListener("click", () => startGame("✊"))
-paperbtn.addEventListener("click", () => startGame("✋"))
-scissorsbtn.addEventListener("click", () => startGame("✌"))
-restartBtn.addEventListener("click", restartGame)
-restartBtn.addEventListener("click",() => {modal.close();})
-
+// Function to start game which plays rounds of RPS, updating scores each round until game over requirements are met
 function startGame(playerSelection){
   const computerSelection = getComputerChoice();
   playRound(playerSelection, computerSelection)
   updateScore()
-  isGameOver()
-  
+  isGameOver() 
 }
 
